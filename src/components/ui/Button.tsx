3d137ed41f0a2b338c54035b1d0da877e0a96865
sujal_01 <sujalpatelyo1042@gmail.com>
2025-04-28 18:02:01 +0530

    added pointer in button

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -11,7 +11,7 @@ const Button: React.FC<ConnectButtonProps> = ({ label, onClick }) => {
   return (
     <div className="bnrbtn transition-all duration-300 relative ease-in-out transform hover:scale-105">
       <button
-        className="w-auto flex justify-center p-3 relative border-white rounded-lg bg-gradient-to-b from-[#bc9b54] to-[#b79249] hover:from-[#e5a526] hover:to-custom-dark-end"
+        className="w-auto cursor-pointer flex justify-center p-3 relative border-white rounded-lg bg-gradient-to-b from-[#bc9b54] to-[#b79249] hover:from-[#e5a526] hover:to-custom-dark-end"
         type="button"
         onClick={onClick}
       >
